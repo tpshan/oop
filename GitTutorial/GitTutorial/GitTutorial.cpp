a3993379212d88b0a1da740518883048abef1189
ntut105650031 <appymhs5049@gmail.com>
2018-03-16 10:03:20 +0800

    test

---
+++ GitTutorial/GitTutorial/GitTutorial.cpp
@@ -0,0 +1,11 @@
+// GitTutorial.cpp: 主要專案檔。
+#include "stdafx.h"
+#include <string>
+#include <iostream>
+using namespace System;
+bool Guess(int number)
+{
+}
+int main(array<System::String^>^ args)
+{
+}
